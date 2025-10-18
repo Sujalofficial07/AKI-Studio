702ab42b552d6a41f519a01b37f3e64646f827ea
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-18 21:06:26 +0530

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // Add any additional repositories here if needed
+        // maven { url = uri("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "Aki Studio"
+include(":app")
