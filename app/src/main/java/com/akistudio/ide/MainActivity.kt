f7d50e2c88223c31144abf7d0e1d969bb85600c7
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-19 08:23:14 +0530

    Update MainActivity.kt

--- app/src/main/java/com/akistudio/ide/MainActivity.kt
+++ app/src/main/java/com/akistudio/ide/MainActivity.kt
@@ -3,17 +3,21 @@ package com.akistudio.ide
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 import com.akistudio.ide.ui.theme.AkiStudioTheme
 import com.akistudio.ide.ui.screens.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        
         setContent {
-            var isDarkMode by remember { mutableStateOf(false) }
+            var isDarkMode by remember { mutableStateOf(isSystemInDarkTheme()) }
             var isEyeProtectionMode by remember { mutableStateOf(false) }
             
             AkiStudioTheme(
