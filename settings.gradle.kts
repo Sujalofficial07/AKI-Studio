a2774533122ba696e1e0ac04d3c3c44c6388e047
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-24 18:27:29 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,24 +5,27 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-   versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
     repositories {
         google()
         mavenCentral()
     }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
 }
+
 rootProject.name = "AkiStudio"
 
 include(":app")
+include(":core")
+include(":ui")
 include(":feature_editor")
 include(":feature_filemanager")
 include(":feature_terminal")
 include(":feature_builder")
-include(":core")
-include(":ui")
