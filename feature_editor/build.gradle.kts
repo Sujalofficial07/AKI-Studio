321185ab2d5184d99abe1b6db9157f0f73498bd2
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-24 19:52:55 +0530

    Update build.gradle.kts

--- feature_editor/build.gradle.kts
+++ feature_editor/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlinOptions { jvmTarget = "21" }
+    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
