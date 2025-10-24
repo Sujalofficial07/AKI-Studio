55e895c4e82a7718da54260b4725a454b118d9e5
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-24 19:33:10 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,3 @@
-plugins {
-    id("com.android.application") version "8.2.2"
-    id("org.jetbrains.kotlin.android") version "1.9.10"
-    id("com.google.dagger.hilt.android") version "2.52"
-    id("org.jetbrains.kotlin.kapt") version "1.9.10"
-}
-
 tasks.named<Delete>("clean") {
     delete(layout.buildDirectory)
 }
