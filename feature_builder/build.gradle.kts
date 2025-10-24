4802eb42a604a5eacc5a5537ea11c937ed6a0fe5
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-24 19:42:33 +0530

    Update build.gradle.kts

--- feature_builder/build.gradle.kts
+++ feature_builder/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
+    id("com.android.application") version "8.2.2"
+    id("org.jetbrains.kotlin.android") version "1.9.10"
+    id("org.jetbrains.kotlin.kapt") version "1.9.10"
+    id("com.google.dagger.hilt.android") version "2.52"
 }
 
 android {
