714cc54ecb5079f164f32368294117ba59fcfa97
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-10-24 20:44:01 +0530

    Update build.gradle.kts

--- feature_filemanager/build.gradle.kts
+++ feature_filemanager/build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
+    implementation("com.google.dagger:hilt-android:2.52")
+    kapt("com.google.dagger:hilt-compiler:2.52")
 
     // SAF + DocumentFile
     implementation("androidx.documentfile:documentfile:1.0.1")
